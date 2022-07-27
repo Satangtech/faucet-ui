b09111c784792f16a317dc22a0a4ecd8f6d670ff
Ic3Sandy <monkey_king2@hotmail.com>
2022-07-27 16:33:48 +0700

    🚧 Modify func App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import "./App.css";
 import BasicCard from "./components/Card";
 import ButtonAppBar from "./components/Navber";
+import Typography from "@mui/material/Typography";
 
 function App() {
   return (
-    <div>
-      <header className="App-header">
-        <ButtonAppBar />
-        <BasicCard />
-      </header>
-    </div>
+    <Typography component="div" className="App">
+      <ButtonAppBar />
+      <BasicCard />
+    </Typography>
   );
 }
 
